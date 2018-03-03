674e792afb1bd8c21c6724c07ac07f37f2f1bf94
charlesbrunson <>
2018-03-03 12:10:52 -0600

    stuff

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,10 @@
 // Game class
 #include "ResourceLoader.hpp"
 #include "Game.hpp"
-#include "Configuration.hpp"
 #include "Log.hpp"
 
-int main(int argc, char* argv[]) {
+int main(int /*argc*/, char** /*argv*/) {
 	
-
-	if (argc >= 1) {
-		Config::setExecPath(argv[0]);
-		Log::msg("Exec location: " + std::string(argv[0]));
-	}
-	
-
 	// Start up the game loop
 	{
 		if (RL()->loadResources()) {
